cfd4f3009a2cb07aa3a86f2a43f28bf4af963ab4
timsixth <timsixth@op.pl>
2024-06-20 10:51:49 +0200

    Fixed UniversalItemMeta on 1.20.4 version and lower versions

--- src/main/java/pl/timsixth/vouchers/util/UniversalItemMeta.java
+++ src/main/java/pl/timsixth/vouchers/util/UniversalItemMeta.java
@@ -8,55 +8,40 @@ import java.lang.reflect.Method;
 
 @RequiredArgsConstructor
 public class UniversalItemMeta {
-
-    private final VersionMatcher versionMatcher = VersionMatcher.getInstance();
-
     private final ItemMeta meta;
 
     public boolean hasLocalizedName() {
-        if (versionMatcher.isSupported("1.20.5")) {
-            try {
-                Method method = meta.getClass().getMethod("hasItemName");
-                method.setAccessible(true);
+        try {
+            Method method = meta.getClass().getMethod("hasItemName");
+            method.setAccessible(true);
 
-                return (boolean) method.invoke(meta);
+            return (boolean) method.invoke(meta);
 
-            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            return meta.hasLocalizedName();
         }
-
-        return meta.hasLocalizedName();
     }
 
     public void setLocalizedName(String localizeName) {
-        if (versionMatcher.isSupported("1.20.5")) {
-            try {
-                Method method = meta.getClass().getMethod("setItemName", String.class);
-                method.setAccessible(true);
+        try {
+            Method method = meta.getClass().getMethod("setItemName", String.class);
+            method.setAccessible(true);
 
-                method.invoke(meta, localizeName);
-            } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                throw new RuntimeException(e);
-            }
+            method.invoke(meta, localizeName);
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            meta.setLocalizedName(localizeName);
         }
-
-        meta.setLocalizedName(localizeName);
     }
 
     public String getLocalizedName() {
-        if (versionMatcher.isSupported("1.20.5")) {
-            try {
-                Method method = meta.getClass().getMethod("getItemName");
-                method.setAccessible(true);
+        try {
+            Method method = meta.getClass().getMethod("getItemName");
+            method.setAccessible(true);
 
-                return (String) method.invoke(meta);
-            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
+            return (String) method.invoke(meta);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            return meta.getLocalizedName();
         }
-
-        return meta.getLocalizedName();
     }
 
     public ItemMeta toItemMeta() {
